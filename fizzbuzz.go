1fba722020f4f9393f6b97fe3855081c6c4576e6
narikin <yuki.iwnr@gmail.com>
2016-09-09 14:31:44 +0900

    implement fizzbuzz.go

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -5,7 +5,15 @@ import (
 )
 
 func fizzbuzz(n int) string {
-	return strconv.Itoa(n)
+	if n%15 == 0 {
+		return "fizzbuzz"
+	} else if n%3 == 0 {
+		return "fizz"
+	} else if n%5 == 0 {
+		return "buzz"
+	} else {
+		return strconv.Itoa(n)
+	}
 }
 
 func main() {
