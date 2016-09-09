6964193220a3977990b35173bbb6aee85efec4cf
narikin <yuki.iwnr@gmail.com>
2016-09-09 14:11:35 +0900

    add test

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -7,4 +7,7 @@ import (
 
 func Test_fizzbuzz_1(t *testing.T) {
 	assert.Equal(t, fizzbuzz("1"), "1", "")
+	assert.Equal(t, fizzbuzz("3"), "fizz", "")
+	assert.Equal(t, fizzbuzz("5"), "buzz", "")
+	assert.Equal(t, fizzbuzz("15"), "fizzbuzz", "")
 }
